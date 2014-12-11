57dda818d0e7e472196338fc5b4f6414bd6a6cb5
aprabhu41190 <ajayprabhu41190@gmail.com>
2014-12-11 12:05:10 -0500

    Created Warp function

---
+++ warp.m
@@ -0,0 +1,12 @@
+function warped_image = warp(image,warp_param,template)
+
+templateX=size(template,2);
+templateY=size(template,1);
+
+%Defining test section
+cropX=templateX*scale;
+cropY=templateY*scale;
+warped_image=imcrop(image,[warp_param(1) warp_param(2) cropX-1 cropY-1]); %-1 to crop
+%imshow(testSection);
+
+end
