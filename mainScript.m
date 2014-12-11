9f42f76b14fbc99dbc1b328415592493c2aa41f0
schaffer2013 <schaffer2013@gmail.com>
2014-12-10 21:54:23 -0500

    test section definition

--- mainScript.m
+++ mainScript.m
@@ -1,8 +1,22 @@
 clc; clear all; close all;
 
 template=imread('ball.jpg');
+templateX=size(template,2);
+templateY=size(template,1);
+
 wholeImage=imread('table.jpg');
+wholeX=size(wholeImage,2);
+wholeY=size(wholeImage,1);
+
+% Set initial values
+xTrans=0;
+yTrans=0;
+rotDeg=0;
+scale=1;
 
 p=[xTrans, yTrans, rotDeg, scale]; %p vector for translation in x, y, rotation in deg, and scale
 
-testSection=
+cropXMax=xTrans+templateX*scale;
+cropYMax=yTrans+templateY*scale;
+testSection=imcrop(wholeImage,[xTrans yTrans cropXMax cropYMax]);
+imshow(testSection);
