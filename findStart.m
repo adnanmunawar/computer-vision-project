1396b7eaba64bb229752e54148a59d050b475044
schaffer2013 <schaffer2013@gmail.com>
2014-12-11 16:05:00 -0500

    FindStart

--- findStart.m
+++ findStart.m
@@ -10,25 +10,28 @@ foundPix=0;
 xTotal=0;
 yTotal=0;
 
-distance=zeros(width,length);
+distanceArray=zeros(width,length);
 for w=1:width
     for l=1:length
-        P=[fullImage(l,w,1),fullImage(l,w,2),fullImage(l,w,3)];
-        distance(w,l)=((P(1)-R)^2+(P(2)-G)^2+(P(3)-B)^2);
-        distance(w,l)=sqrt(double(distance(w,l)));
-        if distance(w,l)<5
+        P=double([fullImage(l,w,1),fullImage(l,w,2),fullImage(l,w,3)]);
+        distanceArray(w,l)=((P(1)-R)^2+(P(2)-G)^2+(P(3)-B)^2);
+        distanceArray(w,l)=sqrt(distanceArray(w,l));
+        if distanceArray(w,l)<40
             xTotal=xTotal+w;
             yTotal=yTotal+l;
             foundPix=foundPix+1;
+            %distanceArray(w,l)=1;
+        else
+            %distanceArray(w,l)=0;
         end
     end
-end
-if foundPix>0
-    xAvg=int16(xTotal/foundPix);
-    yAvg=int16(yTotal/foundPix);
-else
-    xAvg=0;
-    yAvg=0;
-end
-Avg=[xAvg yAvg];
+    if foundPix>0
+        xAvg=int16(xTotal/foundPix);
+        yAvg=int16(yTotal/foundPix);
+    else
+        xAvg=0;
+        yAvg=0;
+    end
+    surf(-distanceArray)
+    Avg=[xAvg yAvg];
 end
