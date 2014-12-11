0a659ff0ee9b17bddd8a643b12de39e9dfbfac76
schaffer2013 <schaffer2013@gmail.com>
2014-12-11 12:12:20 -0500

    Starting Gradient

--- mainScript.m
+++ mainScript.m
@@ -11,42 +11,40 @@ wholeY=size(wholeImage,1);
 % Set initial values
 xTrans=197;
 dx=5;
-yTrans=167;
+yTrans=137;
 dy=5;
 rotDeg=0;
 dRot=0;
 scale=1;
 dScale=0;
-windowSize=25;
-upDown=(windowSize+1)/2;
-SSE=zeros(windowSize)+1000000;
-minError=inf;
-for m=1:1:windowSize
-    for n=1:1:windowSize
-        
-        p=[xTrans+3*(m-upDown), yTrans+3*(n-upDown), rotDeg, scale]; %p vector for translation in x, y, rotation in deg, and scale
 
-        dp=[dx, dy, dRot,dScale];
-                
-        %Defining test section
-        cropX=templateX*scale;
-        cropY=templateY*scale;
-        testSection=imcrop(wholeImage,[p(1) p(2) cropX-1 cropY-1]); %-1 to crop
-        imshow(testSection);
-        
-        %Difference Image
-        diffIm=im2double(testSection-template);
-        %imshow(testSection-template);
-        imageError=0; %initialize error
-        for x=1:1:templateX
-            for y=1:1:templateY
-                imageError=diffIm(x,y)^2+imageError;
-            end
-        end
-        
-        SSE(m,n)=imageError;
-        %p=p+dp;
+p=[xTrans, yTrans, rotDeg, scale]; %p vector for translation in x, y, rotation in deg, and scale
+
+dp=[dx, dy, dRot,dScale];
+
+%Defining test section
+cropX=templateX*scale;
+cropY=templateY*scale;
+testSection=imcrop(wholeImage,[p(1) p(2) cropX-1 cropY-1]); %-1 to crop
+%imshow(testSection);
+
+%Difference Image
+diffIm=im2double(testSection-template);
+%imshow(testSection-template);
+imageError=0; %initialize error
+for x=1:1:templateX
+    for y=1:1:templateY
+        imageError=diffIm(x,y)^2+imageError;
     end
-    
 end
-surf(1:m,1:n,-SSE) %Show negative error (to see a peak)
+
+p=p+dp;
+
+wholeImageBW=rgb2gray(wholeImage);
+[wholeGx, wholeGy]=imgradientxy(wholeImageBW);
+
+testSectionBW=rgb2gray(testSection);
+[Gx,Gy]=imgradientxy(testSectionBW);
+
+imshow(Gy)
+
