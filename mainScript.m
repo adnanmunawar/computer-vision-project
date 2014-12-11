b8edfb6d044295c91763b617a85cc4fb63e2306b
schaffer2013 <schaffer2013@gmail.com>
2014-12-10 22:17:35 -0500

    Image Diff

--- mainScript.m
+++ mainScript.m
@@ -10,7 +10,7 @@ wholeY=size(wholeImage,1);
 
 % Set initial values
 xTrans=200;
-yTrans=200;
+yTrans=180;
 rotDeg=0;
 scale=1;
 
@@ -19,6 +19,17 @@ p=[xTrans, yTrans, rotDeg, scale]; %p vector for translation in x, y, rotation i
 %Defining test section
 cropX=templateX*scale;
 cropY=templateY*scale;
-testSection=imcrop(wholeImage,[xTrans yTrans cropX cropY]);
-imshow(testSection);
+testSection=imcrop(wholeImage,[xTrans yTrans cropX-1 cropY-1]); %-1 to crop
+%imshow(testSection);
+
+%Difference Image
+diffIm=testSection-template;
+imshow(diffIm)
+diffIm=im2double(diffIm);
+imageError=0; %initialize error
+for x=1:1:templateX
+    for y=1:1:templateY
+        imageError=diffIm(x,y)^2+imageError;
+    end
+end
 
