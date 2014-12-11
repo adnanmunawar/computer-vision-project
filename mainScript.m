eda0e82044caffed76d695cc5ed9da4996b90418
schaffer2013 <schaffer2013@gmail.com>
2014-12-10 21:39:06 -0500

    Starting off

--- mainScript.m
+++ mainScript.m
@@ -1,1 +1,8 @@
 clc; clear all; close all;
+
+template=imread('ball.jpg');
+wholeImage=imread('table.jpg');
+
+p=[xTrans, yTrans, rotDeg, scale]; %p vector for translation in x, y, rotation in deg, and scale
+
+testSection=
