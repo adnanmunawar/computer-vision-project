cd6e5c70aaaf5582060a0c15c5ca462b8979318c
aprabhu41190 <ajayprabhu41190@gmail.com>
2014-12-12 13:22:51 -0500

    Computing error matrix

--- Test Code/main.m
+++ Test Code/main.m
@@ -1,13 +1,16 @@
 clc; clear all; close all;
 
+%Loading the template
 template=imread('ball.jpg');
 templateX=size(template,2);
 templateY=size(template,1);
 
+%Loading the image
 wholeImage=imread('floor.jpg');
 wholeX=size(wholeImage,2);
 wholeY=size(wholeImage,1);
 
+%Finding inital point for search
 initialTrans=findStart(wholeImage);
 
 % Set initial values
@@ -15,8 +18,18 @@ xTrans=initialTrans(1);
 yTrans=initialTrans(2);
 scale=1;
 
+%Warp matrix
 p=[scale,0,xTrans ; 0,scale,yTrans];
 
+%Step1 - Warping the image
 testSection=warp_image(wholeImage,p,template);
 figure
 imshow(testSection);
+
+%Step2 - Computing the error
+diffIm=im2double(testSection-template);
+diffIm2=im2double(rgb2gray(testSection)-rgb2gray(template));
+figure;
+imshow(diffIm2) ;
+
+
