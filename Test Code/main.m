6d3f4e20d335a955dbf71e21f1f2300979f2badd
aprabhu41190 <ajayprabhu41190@gmail.com>
2014-12-12 13:06:36 -0500

    New Test code Folder
    Testing the whole code step by step
    Step 1- reading the images

---
+++ Test Code/main.m
@@ -0,0 +1,9 @@
+clc; clear all; close all;
+
+template=imread('ball.jpg');
+templateX=size(template,2);
+templateY=size(template,1);
+
+wholeImage=imread('floor.jpg');
+wholeX=size(wholeImage,2);
+wholeY=size(wholeImage,1);
