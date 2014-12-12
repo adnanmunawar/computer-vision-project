d9c38cf4d8ac302a771df1e31843c434cc0c334c
aprabhu41190 <ajayprabhu41190@gmail.com>
2014-12-11 21:08:57 -0500

    Added Hessian and subhessian

--- Hessian.m
+++ Hessian.m
@@ -1,13 +1,21 @@
 function hess = Hessian(steepest)
 hessX = size(steepest,2) ;
-hessY = size(steepest,1) ;
-hess =zeros(hessX);
+s_hess =zeros(3);
+hess = zeros(hessX) ;
+matrixS = steepest' ;
 
-for countx=0:2
-    for county=0:2
-        hess(countx+1,county+1) = steepest(hessY,(county*hessX/3)+1)' * steepest(hessY,(countx*hessX/3)+1) ;
+for countx=1:3
+    for county=1:3
+        matrixA = steepest(:,1:countx*hessX/3) ;
+        matrixB = matrixS(1:county*hessX/3,:) ;
+        s_hess(countx,county) = subhessian(matrixB,matrixA) ;
     end
 end
+hess = [ ones(hessX/3) * s_hess(1,1) , ones(hessX/3) * s_hess(1,2) , ones(hessX/3) * s_hess(1,3)  ;
+    ones(hessX/3) * s_hess(2,1) , ones(hessX/3) * s_hess(2,2) , ones(hessX/3) * s_hess(2,3)  ;
+    ones(hessX/3) * s_hess(3,1) , ones(hessX/3) * s_hess(3,2) , ones(hessX/3) *s_hess(3,3)  ] ;
+s_hess ;
+hess = hess/1000 ;
 figure ;
 imshow(hess) ;
 end
--- subhessian.m
+++ subhessian.m
@@ -1,6 +1,6 @@
-function subhess = subhessian(temp1,temp2)
+function total = subhessian(temp1,temp2)
 prod = temp1*temp2 ;
 total = sum(sum(prod)) ; 
-subhess = ones(size(prod,2)) ; 
-subhess = total * subhess ;
+%subhess = ones(size(prod,2)) ; 
+%subhess = total * subhess ;
 end
