dab3f10ac2846e29f9052f918df20a52b60d4041
Adnan Munawar <amunawar@wpi.edu>
2014-12-17 14:24:52 -0500

    Added Function to automatically get template from video

---
+++ Test Code/get_template.m
@@ -0,0 +1,65 @@
+function get_template(image)
+
+imageT_gray = im2bw(image);
+
+imageT_gray_filt = medfilt2(imageT_gray, [5 5]);
+
+imageT_gray_filt_bw = im2bw(imageT_gray_filt);
+
+%corner are set to 0 after the 2D filtering above so explicitly setting
+%them to zero
+
+imageT_gray_filt_bw(1:5,1:5) = 1;
+imageT_gray_filt_bw(1:5,end-5:end) = 1;
+imageT_gray_filt_bw(end-5:end,1:5) = 1;
+imageT_gray_filt_bw(end-5:end,end-5:end) = 1;
+
+%Dropping the naming convention to get a shorter image name
+
+imageT = imageT_gray_filt_bw;
+
+imshow(imageT);
+figure;
+
+[topCorner, bottomCorner] = find_ball_clipping_xy(imageT);
+
+template = imcrop(image, [topCorner(1)...
+    topCorner(2) ...
+    bottomCorner(1)-topCorner(1) ...
+    bottomCorner(2)-topCorner(2)]);
+
+imshow(template);
+
+
+
+end
+
+function[topCorner, bottomCorner] =  find_ball_clipping_xy(image)
+
+[startX, endX] =  get_x_clip(image);
+[startY, endY] =  get_y_clip(image);
+
+topCorner = [startX startY];
+bottomCorner = [endX endY];
+
+end
+
+function[startX_clip, endX_clip] =  get_x_clip(image)
+
+[startY_clip, startX_clip] = find((image == 0),1,'first');
+
+[endY_clip, endX_clip] = find((image == 0), 1, 'last');
+
+end
+
+
+
+function[startY_clip, endY_clip] =  get_y_clip(image)
+
+image = image';
+
+[startX_clip, startY_clip] = find((image == 0), 1,'first');
+
+[endX_clip, endY_clip] = find((image == 0), 1, 'last');
+
+end
